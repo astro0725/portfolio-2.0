318dd691fe8e742307c7650cb3ae1dc9c897c9a1
astro0725 <angelica.strong0725@gmail.com>
2024-10-09 12:37:09 -0500

    imported pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import About from './pages/About/Blurb.jsx';
+import Portfolio from './pages/Portfolio/Repos.jsx';
+import Resume from './pages/Resume/CV.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <About/>,
+      },
+      {
+        path: '/resume',
+        element: <Resume/>,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio/>,
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
