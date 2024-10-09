561f2ff9ea59f42911c7c1fce6f49c227e49ddac
astro0725 <angelica.strong0725@gmail.com>
2024-10-09 17:41:07 -0500

    updated navbar spacing for mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='items-center text-white'>
-      <nav className='bg-primary p-4 text-gray-300 rounded-t-lg'>
-      <ul className='flex flex-wrap justify-center just items-center px-10'>
-        <li>
-          <NavLink to='/' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
-            About
-          </NavLink>
-        </li>
+    <div className='text-white'>
+      <nav className='flex justify-center items-center bg-primary p-4 text-gray-300 rounded-t-lg'>
+        <ul className='flex flex-col sm:flex-row justify-center items-center sm:grid sm:grid-cols-3 px-10'>
+          <li className='my-2 sm:my-0'>
+            <NavLink to='/' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
+              About
+            </NavLink>
+          </li>
 
-        <li>
-          <NavLink to='/resume' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
-            Resume
-          </NavLink>
-        </li>
+          <li className='my-2 sm:my-0'>
+            <NavLink to='/resume' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
+              Resume
+            </NavLink>
+          </li>
 
-        <li>
-          <NavLink to='/portfolio' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
-            Portfolio
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  </div>
-  )
-}
+          <li className='my-2 sm:my-0'>
+            <NavLink to='/portfolio' className='text-white text-lg font-bold px-7 transition-colors duration-300 hover:text-tertiary' data-nav-link>
+              Portfolio
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
